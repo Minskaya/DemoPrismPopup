91d5cd4a7fae1210cb3c6f943e7e6dee021922df
Minskaya <Minskaya@XPS.(none)>
2017-12-17 11:49:20 +0100

    Add project files.

---
+++ DemoPrismPopup/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DemoPrismPopup
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return new MainWindow();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow = (Window)Shell;
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ DemoPrismPopup/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DemoPrismPopup
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _nom;
+
+        public MainWindowViewModel(IInteractionService)
+        {
+            this.Nom = "Entrez le nom";
+            this.PopupWithServiceCommand = new DelegateCommand(this.PopupWithServiceCommandExecute);
+            this.MessageBoxCommand = new DelegateCommand(this.MessageBoxCommandExecute);
+        }
+
+        public ICommand MessageBoxCommand { get; set; }
+
+        public string Nom
+        {
+            get
+            {
+                return this._nom;
+            }
+            set
+            {
+                this.SetProperty(ref _nom, value);
+            }
+        }
+
+        public ICommand Popup2 { get; private set; }
+
+        public ICommand PopupWithServiceCommand { get; private set; }
+
+        private void MessageBoxCommandExecute()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PopupWithServiceCommandExecute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
